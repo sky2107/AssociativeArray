b066b51ec5de30fc618a0cd2d13da2b5bf1fc65d
Adrian Endrich <adrian.endrich@googlemail.com>
2014-11-13 18:03:56 +0100

    adding test stubs. Integer and StringAssociativeArrays dont need to be tested. If BinTreeAssociativeArray succeeds, they would have succeded, too.

---
+++ src/com/nsa/associativearray/tests/BinTreeAssociativeArrayTest.java
@@ -0,0 +1,83 @@
+package com.nsa.associativearray.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Adrian Endrich, Marcel Heuser, Felix Navas
+ *
+ */
+public class BinTreeAssociativeArrayTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testForEach() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExtractAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMap() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/nsa/associativearray/tests/DictionaryTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.nsa.associativearray.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Adrian Endrich, Marcel Heuser, Felix Navas
+ *
+ */
+public class DictionaryTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.nsa.associativearray.implementation.Dictionary#keys()}.
+	 */
+	@Test
+	public void testKeys() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.nsa.associativearray.implementation.Dictionary#values()}.
+	 */
+	@Test
+	public void testValues() {
+		fail("Not yet implemented");
+	}
+
+}
