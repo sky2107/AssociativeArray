5cd5e9fbee7aeb02eef2674be20d94a7c139c67a
Adrian Endrich <adrian.endrich@googlemail.com>
2014-11-13 17:56:45 +0100

    adding comments in Dictionary.java

--- src/com/nsa/associativearray/implementation/Dictionary.java
+++ src/com/nsa/associativearray/implementation/Dictionary.java
@@ -8,11 +8,19 @@ package com.nsa.associativearray.implementation;
 
 public class Dictionary extends StringAssociativeArray<String> {
 
+	/**
+	 * returns all keys in a String array.
+	 * @return array of keys
+	 */
 	public String[] keys() {
 		// TODO
 		return null;
 	}
 
+	/**
+	 * returns all values in a String array.
+	 * @return array of values
+	 */
 	public String[] values() {
 		// TODO
 		return null;
